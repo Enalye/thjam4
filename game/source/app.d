7573c3dd55167a32c2a2c685aac9d569d9feb9e5
Enalye <enalyen@gmail.com>
2019-06-21 00:28:26 +0200

    Initial B commit

--- game/source/app.d
+++ game/source/app.d
@@ -1,6 +1,28 @@
-import std.stdio;
 
-void main()
-{
-	writeln("Edit source/app.d to start your project.");
+/**
+    Test application
+
+    Copyright: (c) Enalye 2017
+    License: Zlib
+    Authors: Enalye
+*/
+
+import std.stdio: writeln;
+
+import atelier;
+
+void main() {
+	try {
+        //Set data location
+        setResourceFolder("../data/");
+        setResourceSubFolder!Texture("graphic");
+        setResourceSubFolder!Font("font");
+        setResourceSubFolder!Sprite("graphic");
+        
+		createApplication(Vec2u(800u, 600u));
+		runApplication();
+	}
+	catch(Exception e) {
+		writeln(e.msg);
+	}
 }
