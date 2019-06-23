f76947aab630d2214077423b26efe476bf854c4d
Enalye <enalyen@gmail.com>
2019-06-23 20:28:22 +0200

    Add tuto + credits

--- game/source/menu.d
+++ game/source/menu.d
@@ -103,6 +103,28 @@ final class IntroGui3: GuiElement {
     override void onEvent(Event event) {
         if(event.type == EventType.MouseDown) {
             removeRootGuis();
+            addRootGui(new TutoGui);
+        }
+    }
+
+    override void draw() {
+        _sprite.draw(center);
+    }
+}
+
+final class TutoGui: GuiElement {
+    private {
+        Sprite _sprite;
+    }
+
+    this() {
+        _sprite = fetch!Sprite("tuto");
+        size = _sprite.size;
+    }
+
+    override void onEvent(Event event) {
+        if(event.type == EventType.MouseDown) {
+            removeRootGuis();
             addRootGui(new MenuGui);
         }
     }
@@ -122,16 +144,37 @@ final class MenuGui: GuiElement {
         _bg = fetch!Sprite("alice.menu");
         _bg.fit(size);
 
-        auto btn = new StartButton;
-        btn.setAlign(GuiAlignX.Left, GuiAlignY.Bottom);
-        btn.position = Vec2f(150f, 150f);
-        btn.setCallback(this, "start");
-        addChildGui(btn);
+        {
+            auto btn = new StartButton;
+            btn.setAlign(GuiAlignX.Left, GuiAlignY.Bottom);
+            btn.position = Vec2f(150f, 150f);
+            btn.setCallback(this, "start");
+            addChildGui(btn);
+        }
+        {
+            auto btn = new TextButton(fetch!Font("VeraMono"), "tutorial");
+            btn.setAlign(GuiAlignX.Left, GuiAlignY.Bottom);
+            btn.position = Vec2f(150f, 50f);
+            btn.setCallback(this, "tuto");
+            addChildGui(btn);
+        }
+
+        {
+            auto txt = new Text;
+            txt.setAlign(GuiAlignX.Right, GuiAlignY.Bottom);
+            txt.position = Vec2f(50f, 50f);
+            txt.text = "Crédits:{n}{n}  Automn'{n}  JonathKane{n}  Enalye{n}  MrNuage";
+            addChildGui(txt);
+        }
     }
 
     override void onCallback(string id) {
         if(id == "start") {
             onSceneStart();
+        }
+        else if(id == "tuto") {
+            removeRootGuis();
+            addRootGui(new TutoGui);
         }
     }
 
