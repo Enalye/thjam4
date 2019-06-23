8ba9ebb6efb08414fdd7f3ec030fcfef300efb9c
= <mickael.olivier@amadeus.com>
2019-06-23 18:41:22 +0200

    Doll animations!

--- game/source/player.d
+++ game/source/player.d
@@ -42,13 +42,13 @@ final class Player: Entity {
 
         dolls = new DollArray();
 
-        dolls.push(new Doll(this, _position, Color.fromRGB(0xFF4500), DollType.SHOT)); // Red
-        dolls.push(new Doll(this, _position, Color.fromRGB(0xFFA500), DollType.LASER)); // Orange
-        dolls.push(new Doll(this, _position, Color.fromRGB(0xFFFF33), DollType.EXPLOSIVE)); // Yellow
-        dolls.push(new Doll(this, _position, Color.fromRGB(0x32CD32), DollType.LANCE, 1000f)); // Green
-        dolls.push(new Doll(this, _position, Color.fromRGB(0x00BFFF), DollType.TELEPORT, 750f)); // Blue
-        dolls.push(new Doll(this, _position, Color.fromRGB(0x4169E1), DollType.BOOMERANG)); // Indigo
-        dolls.push(new Doll(this, _position, Color.fromRGB(0xBA55D3), DollType.SHIELD)); // Purple
+        dolls.push(new Doll("doll", this, _position, Color.fromRGB(0xFF4500), DollType.SHOT)); // Red
+        dolls.push(new Doll("doll", this, _position, Color.fromRGB(0xFFA500), DollType.LASER)); // Orange
+        dolls.push(new Doll("doll", this, _position, Color.fromRGB(0xFFFF33), DollType.EXPLOSIVE)); // Yellow
+        dolls.push(new Doll("doll", this, _position, Color.fromRGB(0x32CD32), DollType.LANCE, 1000f)); // Green
+        dolls.push(new Doll("doll", this, _position, Color.fromRGB(0x00BFFF), DollType.TELEPORT, 750f)); // Blue
+        dolls.push(new Doll("doll", this, _position, Color.fromRGB(0x4169E1), DollType.BOOMERANG)); // Indigo
+        dolls.push(new Doll("doll", this, _position, Color.fromRGB(0xBA55D3), DollType.SHIELD)); // Purple
 
 
         _currentDoll = dolls[_dollIndex];
