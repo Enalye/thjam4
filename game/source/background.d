a84e991fdc9183271fc9f295c5a7aeff18cd6374
Enalye <enalyen@gmail.com>
2019-06-23 16:04:02 +0200

    Parallax

--- game/source/background.d
+++ game/source/background.d
@@ -2,6 +2,8 @@ module game.background;
 
 import atelier;
 
+import game.camera, game.level, game.global;
+
 final class Background {
     private {
         Sprite _bg;
@@ -10,10 +12,16 @@ final class Background {
     this() {
         _bg = fetch!Sprite("level.foret");
         _bg.scale = Vec2f.one * 2f;
-        _bg.anchor = Vec2f(0f, 1f);
     }
 
     void draw() {
-        _bg.draw(Vec2f.zero);
+        auto camPos = getCameraPosition();
+        float t1 = rlerp(0f, currentLevel.clampWidth, camPos.x);
+        float t2 = rlerp(-1500f, 0f, camPos.y);
+        Vec2f pos = Vec2f(
+            lerp(_bg.size.x / 2f, currentLevel.clampWidth - _bg.size.x / 2f, t1),
+            lerp(-1500f + _bg.size.y / 2f, 0f, t2)
+        );
+        _bg.draw(pos);
     }
 }
