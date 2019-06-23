a2e75aeadfa56bfce3e236b4d4cfb52ca9386223
= <mickael.olivier@amadeus.com>
2019-06-23 02:50:41 +0200

    Trying to make opaque data work

--- game/source/coroutils.d
+++ game/source/coroutils.d
@@ -8,17 +8,18 @@ import grimoire;
 import game.global, game.entity, game.shot, game.enemy;
 
 void addPrimitives() {
+	auto grEnemy = grAddUserType("Enemy");
 	grAddPrimitive(&grCos, "cos", ["value"], [grFloat], [grFloat]);
 	grAddPrimitive(&grSin, "sin", ["value"], [grFloat], [grFloat]);
 	grAddPrimitive(&grPosSin, "psin", ["value"], [grFloat], [grFloat]);
 	grAddPrimitive(&grPrint, "print", ["value"], [grString]);
 	grAddPrimitive(&grSetColor, "setColor", ["r", "g", "b"],  [grFloat, grFloat, grFloat]);
-	grAddPrimitive(&grSpawnEnemy, "spawnEnemy", ["index", "name", "x", "y"], [grInt, grString, grFloat, grFloat]);
-	grAddPrimitive(&grFireShot, "fireShot", ["index", "x", "y", "angle", "speed"], [grInt, grFloat, grFloat, grFloat, grFloat]);
-	grAddPrimitive(&grGetPosition, "getPosition", ["index"], [grInt], [grFloat, grFloat]);
-	grAddPrimitive(&grSetPosition, "setPosition", ["index", "x", "y"], [grInt, grFloat, grFloat]);
-	grAddPrimitive(&grSetMovementSpeed, "setMovementSpeed", ["index", "movX", "movY"], [grInt, grFloat, grFloat]);
-	grAddPrimitive(&grIsAlive, "isAlive", ["index"], [grInt], [grBool]);
+	grAddPrimitive(&grSpawnEnemy, "spawnEnemy", ["name", "x", "y"], [grString, grFloat, grFloat], [grEnemy]);
+	grAddPrimitive(&grFireShot, "fireShot", ["enemy", "x", "y", "angle", "speed"], [grEnemy, grFloat, grFloat, grFloat, grFloat]);
+	grAddPrimitive(&grGetPosition, "getPosition", ["enemy"], [grEnemy], [grFloat, grFloat]);
+	grAddPrimitive(&grSetPosition, "setPosition", ["enemy", "x", "y"], [grEnemy, grFloat, grFloat]);
+	grAddPrimitive(&grSetMovementSpeed, "setMovementSpeed", ["enemy", "movX", "movY"], [grEnemy, grFloat, grFloat]);
+	grAddPrimitive(&grIsAlive, "isAlive", ["enemy"], [grEnemy], [grBool]);
 }
 
 private void grCos(GrCall call) {
@@ -48,23 +49,21 @@ private void grSetColor(GrCall call) {
 }
 
 private void grSpawnEnemy(GrCall call) {
-	int index   = call.getInt("index");
 	string name = to!string(call.getString("name"));
 	float x     = call.getFloat("x");
 	float y     = call.getFloat("y");
 
-	Enemy enemy = new Enemy(index, name, Vec2f(x, y));
+	Enemy enemy = new Enemy(name, Vec2f(x, y));
 	enemies.push(enemy);
+	call.setUserData(enemy);
 }
 
 private void grFireShot(GrCall call) {
-	int index = call.getInt("index");
-	float x   = call.getFloat("x");
-	float y   = call.getFloat("y");
-	float angle = call.getFloat("angle");
-	float speed = call.getFloat("speed");
-
-	Enemy enemy    = enemies[index];
+	Enemy enemy    = call.getUserData("enemy");
+	float x        = call.getFloat("x");
+	float y        = call.getFloat("y");
+	float angle    = call.getFloat("angle");
+	float speed    = call.getFloat("speed");
 	Vec2f spawnPos = Vec2f(x, y);
 
 	createShot(EntityType.ENEMY,
@@ -78,30 +77,27 @@ private void grFireShot(GrCall call) {
 }
 
 private void grGetPosition(GrCall call) {
-	Enemy enemy    = enemies[call.getInt("index")];
+	Enemy enemy    = call.getUserData("enemy");
 	Vec2f position = enemy.position; 
 	call.setFloat(position.x);
 	call.setFloat(position.y);
 }
 
 private void grSetPosition(GrCall call) {
-	int index = call.getInt("index");
-	float x = call.getFloat("x");
-	float y = call.getFloat("y");
-	Enemy enemy = enemies[index];
+	Enemy enemy    = call.getUserData("enemy");
+	float x        = call.getFloat("x");
+	float y        = call.getFloat("y");
 	enemy.position = Vec2f(x, y);
 }
 
 private void grSetMovementSpeed(GrCall call) {
-	int index = call.getInt("index");
-	float movX = call.getFloat("movX");
-	float movY = call.getFloat("movY");
-	Enemy enemy = enemies[index];
+	Enemy enemy = call.getUserData("enemy");
+	float movX  = call.getFloat("movX");
+	float movY  = call.getFloat("movY");
 	enemy.movementSpeed = Vec2f(movX, movY);
 }
 
 private void grIsAlive(GrCall call) {
-	int index = call.getInt("index");
-	Enemy enemy = enemies[index];
+	Enemy enemy = call.getUserData("enemy");
 	call.setBool(enemy.isAlive());
 }
